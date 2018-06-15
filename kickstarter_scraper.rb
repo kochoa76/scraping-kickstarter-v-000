cad3e434fd270be9806c3b64138ce7e2d5f6036c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-15 18:55:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,13 +16,13 @@ def create_project_hash
 
     kickstarter.css("li.project.grid_4").each |project|
     title = project.css("h2.bbcard_name strong a").text
-      projects[title.to_sym]={
+      projects[title.to_sym]= {
           :img_link => project.css("div.project-thumbnail img").attribute("src").value,
           :description => project.css("div.project-card p.bbcard_blurb").text,
           :location => project.css("span.location-name").text,
-          :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i,
+          :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
         }
     end
       projects
   end
-end 
+
